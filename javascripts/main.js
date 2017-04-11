9ad3f8069571b9651d73eb7ad44b3ac04ccf5d41
Kristin Caron <krissycaron@gmail.com>
2017-04-10 21:41:19 -0500

    need to finish styling that broke

--- javascripts/main.js
+++ javascripts/main.js
@@ -27,18 +27,60 @@ function makeDom(myArrayToPrint){
 	var myDomString = "";
 	 for (var i=0; i<myArrayToPrint.length; i++){
 	 	console.log(myArrayToPrint);
-	 	myDomString += `<div class="dinoCard">`;
-	 	myDomString += `<header><h1>${myArrayToPrint[i].type}<h1></header>`;
-	 	myDomString += `<section>`;
-	 	myDomString += `<img src="${myArrayToPrint[i].img}">`;
-	 	myDomString += `<p class="bio">"${myArrayToPrint[i].bio}"</p>`;
-	 	myDomString += `</section>`;
-	 	myDomString += `<footer>${myArrayToPrint[i].info}</footer>`;
-	 	myDomString +=`</div>`;
+	 	if (i%3 === 0){
+	 		myDomString += `<div class="row">`
+	 	}
+		 	myDomString += `<div class="col-sm-6 col-med-4 dinoCard">`;
+		 	myDomString += `<header><h1>${myArrayToPrint[i].type}<h1></header>`;
+		 	myDomString += `<section>`;
+		 	myDomString += `<img src="${myArrayToPrint[i].img}">`;
+		 	myDomString += `<p class="bio">"${myArrayToPrint[i].bio}"</p>`;
+		 	myDomString += `</section>`;
+		 	myDomString += `<footer>${myArrayToPrint[i].info}</footer>`;
+		 	myDomString +=`</div>`;
+				if(i%3===2){
+				myDomString += `</div>`
+				}
 	}
 	//target to print to dom with jquery
 	//taking the dinosaurs and adding the data to the string. 
 	$("#dinosaurs").append(myDomString);
 }
+
+//click events in jquery Click on the dino card and it will"bubble up" 
+//to encapusulate the enitre card of what we have placed the "click" on "this"
+
+$("#dinosaurs").on("click",".dinoCard", function(e){
+	$(".dinoCard").removeClass("dottedBorder");
+//wrapping it in a jquery object "this"
+	$(this).addClass("dottedBorder");
+//clear textbox value to an empty string,//focus on the box you re-select it. 
+	$("#textbox").val("").focus(); 
+});
+//eventlistener clicker
+$("#textbox").keyup(mirrorText);
+
+function mirrorText(e){
+	var selectedCard = $(".dottedBorder"); //only returns the one selected card ie the one that the card was clicked on 
+	var bioTyped = $("#textbox").val(); //entered information in text box from user
+	var bio = $(".dottedBorder").find("p.bio"); //finding the p in the class bio, we just want the selected div, with that certain tag (ie: <p> in the div) so it looks through all the children of the elements
+	// bio = bioTyped;
+	bio.html(bioTyped); //jquery
+
+	if (e.keyCode === 13){
+		$("#textbox").val("");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
